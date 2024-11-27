131dd1eec34f454d21dca81d3314b98931809993
ln0185 <helene.delaunayratel@gmail.com>
2024-11-27 11:23:46 +0000

    backedfixes

--- src/components/StoriesCard/StoriesCard.tsx
+++ src/components/StoriesCard/StoriesCard.tsx
@@ -7,23 +7,56 @@ interface StoriesCardInterface {
 
 type StoriesCardType = {
   data: StoriesCardInterface;
-  categoryName: string | NavigateOptions,
+  categoryName: string | NavigateOptions;
 };
 
-export const StoriesCard = ({data, categoryName}: StoriesCardType) => {
+export const StoriesCard = ({ data, categoryName }: StoriesCardType) => {
   let stories;
   console.log(data);
   if (data) {
-    stories = Object.keys(data?.stories || data);
+    stories = Object.values(data?.stories || data);
   }
-  
+
+  let navigate = useNavigate();
+
+  const handleStoryClick = (e: string, categoryName: string) => {
+    if (categoryName === "Allt") {
+      categoryName = "all";
+    }
+
+    if (categoryName === "Tröll") {
+      categoryName = "troll";
+    }
+
+    if (categoryName === "Draugar") {
+      categoryName = "draugar";
+    }
+
+    if (categoryName === "alfar-og-huldufolk") {
+      categoryName = "alfa";
+    }
+
+    if (categoryName === "Helgisögur") {
+      categoryName = "ur-efra-og-nedra-helgisogur";
+    }
+
+    // console.log(console.log("Category name", categoryName));
+    navigate(`/stories/${categoryName}/${e}`);
+  };
+
   return (
     <>
       {stories?.slice(0, 3).map((item) => {
         return (
           <figure key={item}>
             <header>
-              <FeaturedStories item={item} categoryName={categoryName}></FeaturedStories>
+              <h2
+                onClick={(e) =>
+                  handleStoryClick(e.target.innerText, categoryName)
+                }
+              >
+                {item == "categories" ? null : item.replace(/[/]/g, "")}
+              </h2>
             </header>
           </figure>
         );
