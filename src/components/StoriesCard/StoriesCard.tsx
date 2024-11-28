a397b738913214c60782a3e26d3969680fa43f75
Chrstjan <ps5christian@hotmail.com>
2024-11-28 16:16:57 +0100

    Removed the white text color

--- src/components/StoriesCard/StoriesCard.tsx
+++ src/components/StoriesCard/StoriesCard.tsx
@@ -67,7 +67,7 @@ export const StoriesCard = ({ data, categoryName }: StoriesCardType) => {
         return (
           <figure key={item}>
             <header>
-              <h2 className="text-white"
+              <h2
                 onClick={(e) =>
                   handleStoryClick(e.target.innerText, categoryName)
                 }
