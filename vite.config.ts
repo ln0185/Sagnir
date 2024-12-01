0529996f78a344a94094147ce3cca47bd90149ff
ln0185 <helene.delaunayratel@gmail.com>
2024-12-01 17:00:08 +0000

    gh livepage fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/M4GroupProject",
+  build: {
+    outDir: "dist", // Ensure this is correct
+  },
 });
